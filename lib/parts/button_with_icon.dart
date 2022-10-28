17d2e447322f86449749af3e73cf3632454b2303
Shintaro Miyazaki <tudopassa427911@gmail.com>
2022-10-28 18:59:17 +0900

    homeScreen

---
+++ lib/parts/button_with_icon.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ButtonWithIcon extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Icon icon;
+  final String label;
+  final Color color;
+
+  const ButtonWithIcon(
+      {required this.onPressed,
+      required this.icon,
+      required this.label,
+      required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 36.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: color,
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ))),
+            onPressed: onPressed,
+            icon: icon,
+            label: Text(label, style: const TextStyle(fontSize: 15.0))),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vocabulary_notebook/parts/button_with_icon.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,11 +17,29 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           Expanded(child: Image.asset("assets/images/image_title.png")),
           _titleText(),
-          // TODO 横線
-          // TODO 確認テストをするボタン
+          const Divider(
+            height: 30.0,
+            indent: 10.0,
+            endIndent: 10.0,
+            color: Colors.white,
+            thickness: 1.0,
+          ),
+          // 確認テストをするボタン
+          ButtonWithIcon(
+            onPressed: () => print("確認テスト"), //TODO
+            icon: const Icon(Icons.play_arrow),
+            label: "確認テストをする",
+            color: Colors.brown,
+          ),
           // TODO ラジオボタン
+          _radioButtons(),
           // TODO 単語一覧を見るボタン
-          Text(
+          ButtonWithIcon(
+              onPressed: () => print("単語一覧"),
+              icon: const Icon(Icons.list),
+              label: "単語一覧を見る",
+              color: Colors.grey),
+          const Text(
             "created by Shintaro",
             style: TextStyle(fontFamily: "Mont"),
           )
@@ -31,7 +50,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   _titleText() {
     return Column(
-      children: [
+      children: const [
         Text(
           "私だけの単語帳",
           style: TextStyle(fontSize: 40.0),
@@ -41,6 +60,32 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 24.0, fontFamily: "Mont"),
         ),
       ],
+    );
+  }
+
+  Widget _radioButtons() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 50.0),
+      child: Column(
+        children: const [
+          RadioListTile(
+              title: Text(
+                "暗記済みの単語を除外する",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              value: null,
+              groupValue: null,
+              onChanged: null),
+          RadioListTile(
+              title: Text(
+                "暗記済みの単語を含む",
+                style: TextStyle(fontSize: 16.0),
+              ),
+              value: null,
+              groupValue: null,
+              onChanged: null)
+        ],
+      ),
     );
   }
 }
